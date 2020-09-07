a1734e7506c72a0d09bf3193bd48ec1ca908d2a1
Nico <nicolas.serrato@outlook.com>
2020-09-07 09:50:02 -0300

    Creates db and the apppointments entity

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -2,25 +2,26 @@ import { Router, request, response } from 'express';
 import { parseISO } from 'date-fns';
 import AppointmentsRepository from './../repositories/AppointmentsRepositoy';
 import CreateAppointmentService from '../services/CreateAppointmentService';
+import { getCustomRepository } from 'typeorm';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRouter.get('/', (request, response) =>{
-  const appointments = appointmentsRepository.all();
+appointmentsRouter.get('/', async (request, response) =>{
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
 });
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
 
     const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService(appointmentsRepository);
+    const createAppointment = new CreateAppointmentService();
 
-    const appointment = createAppointment.execute({provider, date: parsedDate});
+    const appointment = await createAppointment.execute({provider, date: parsedDate});
 
     return response.json(appointment);
   } catch(err) {
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import routes from './routes';
+import 'reflect-metadata';
+import './database';
 
 const app = express();
 
